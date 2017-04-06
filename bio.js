94e1b16e8b0335cd2162590d0188125cb0ca82ea
HannahPoy <hjplsc2@gmail.com>
2017-04-06 11:38:49 -0700

    Update bio.js

--- bio.js
+++ bio.js
@@ -1,6 +1,6 @@
 function ernestinan() {
 	document.getElementById("name").innerHTML = "Ernestina Navarro";
-	document.getElementById("bio").innerHTML = "I am a student at Da Vinci Communications High School. Here are some things that I have been doing in my computer science class.";
+	document.getElementById("bio").innerHTML = "My name is Ernestina Navarro and I’m a senior here at Da Vinci Communications. I’ve been attending  DVC since they first opened back in 2013. I’ve learned a lot being here at this school. I can say that this school has changed me a lot as a person. I feel like it was only yesterday that I was a freshmen here. I was a very shy and quiet person back in my freshmen year. Here at Da Vinci, we have a saying: “We do things Differently.” I didn’t know what that meant my freshmen year, but I can sure say that DVC has certainly lived up to that saying. Doing things differently here means doing things outside the box. Doings thing that will challenge you or you know “prepare” you for college. Being here I learned how to embrace who I was and learned to not be afraid to speak my mind. This school has certainly molded me to be this person that I had yearned to be. I want to grow up majoring in being a preschool teacher. I’ve always liked the idea of working with kids and help mold them for their years in elementary. The school helped me to know that this is what I want to do. I’ve lived four years of my life here, and it’s been quite the journey. This school has presented me with such wonderful people. I’m lucky to have them in  my life. If I hadn’t decided to attend this school, I would have never had the pleasure of meet them. I’ve grown to care much about this school. As much as this school has had it’s downfalls, I can’t completely hate it. It’s crazy, that I’m soon to be graduating soon. But this is what they have been preparing ourselves for. I’m now ready to take that leap and to finally take my final steps here at DVC. Class of 2017, good luck and enjoy life.";
 	document.getElementById("link").href = "index.html";
 	document.getElementById("linkt").innerHTML = "Click here to see my LinkedIn profile.";
 }
@@ -11,14 +11,14 @@ function hannahp() {
 	document.getElementById("linkt").innerHTML = "Click here to see my LinkedIn profile.";
 }
 function jenniferj() {
-	document.getElementById("name").innerHTML = "Jennifer-Lyn Johnson";
-	document.getElementById("bio").innerHTML = "I am a student at Da Vinci Communications High School. Here are some things that I have been doing in my computer science class.";
+	document.getElementById("name").innerHTML = "Jennifer'Lyn Johnson";
+	document.getElementById("bio").innerHTML = "Hello. My Name is Jennifer Johnson. I am currently a junior at Da Vinci Communications. I have attended DVC since my freshman year of high school. The past three years here at Da Vinci have made me a better public speaker and a better collaborator. I was a part of the track team in the year of 2016 for a season and I enjoyed it a lot. I also was able to create my own seminar for a semester. My seminar was a ¨cheer¨ club that encouraged student support at basketball games and soccer games. Before the ¨spirit squad¨ seminar, I did not go to any of the school’s games; I did not even know when the games took place. Once the seminar started, I actually went to the games to support my schoolmates and I enjoyed myself a lot. Overall, my experience here at DVC has been one to remember and I am looking forward to completing my high school experience here and continuing on to more adventures. I will be graduating in the class of 2018.";
 	document.getElementById("link").href = "index.html";
 	document.getElementById("linkt").innerHTML = "Click here to see my LinkedIn profile.";
 }
 function kaim() {
 	document.getElementById("name").innerHTML = "Kai Morris";
-	document.getElementById("bio").innerHTML = "I am a student at Da Vinci Communications High School. Here are some things that I have been doing in my computer science class.";
+	document.getElementById("bio").innerHTML = "My name is Kai Morris and I am currently a junior at Da Vinci Communications. I am currently a part of the Black Student Union (BSU), a club created by two students at Communications. I am a part of the computer science pathway, where we use coding programs to create websites, apps, and program robots to not only move, but dance and perform a series of other actions. I chose to be a part of the computer science because I felt that it would guide me in the direction of my desired career, which lies within the medical field. I will be graduating in the class of 2018.";
 	document.getElementById("link").href = "index.html";
 	document.getElementById("linkt").innerHTML = "Click here to see my LinkedIn profile.";
 }
