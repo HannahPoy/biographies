0421ba479f3bf5b5ee7559474f78a4f4c162968e
HannahPoy <hjplsc2@gmail.com>
2017-04-06 09:35:04 -0700

    Create bio.js

---
+++ bio.js
@@ -0,0 +1,36 @@
+function ernestinan() {
+	document.getElementById("name").innerHTML = "Ernestina Navarro";
+	document.getElementById("bio").innerHTML = "I am a student at Da Vinci Communications High School. Here are some things that I have been doing in my computer science class.";
+	document.getElementById("link").href = "index.html";
+	document.getElementById("linkt").innerHTML = "Click here to see my LinkedIn profile.";
+}
+function hannahp() {
+	document.getElementById("name").innerHTML = "Hannah Poyourow";
+	document.getElementById("bio").innerHTML = "I am a student at Da Vinci Communications High School. Here are some things that I have been doing in my computer science class.";
+	document.getElementById("link").href = "index.html";
+	document.getElementById("linkt").innerHTML = "Click here to see my LinkedIn profile.";
+}
+function jenniferj() {
+	document.getElementById("name").innerHTML = "Jennifer-Lyn Johnson";
+	document.getElementById("bio").innerHTML = "I am a student at Da Vinci Communications High School. Here are some things that I have been doing in my computer science class.";
+	document.getElementById("link").href = "index.html";
+	document.getElementById("linkt").innerHTML = "Click here to see my LinkedIn profile.";
+}
+function kaim() {
+	document.getElementById("name").innerHTML = "Kai Morris";
+	document.getElementById("bio").innerHTML = "I am a student at Da Vinci Communications High School. Here are some things that I have been doing in my computer science class.";
+	document.getElementById("link").href = "index.html";
+	document.getElementById("linkt").innerHTML = "Click here to see my LinkedIn profile.";
+}
+function merita() {
+	document.getElementById("name").innerHTML = "Merit Abshire";
+	document.getElementById("bio").innerHTML = "I am a student at Da Vinci Communications High School. Here are some things that I have been doing in my computer science class.";
+	document.getElementById("link").href = "index.html";
+	document.getElementById("linkt").innerHTML = "Click here to see my LinkedIn profile.";
+}
+function rocioh() {
+	document.getElementById("name").innerHTML = "Rocio Handal";
+	document.getElementById("bio").innerHTML = "I am a student at Da Vinci Communications High School. Here are some things that I have been doing in my computer science class.";
+	document.getElementById("link").href = "index.html";
+	document.getElementById("linkt").innerHTML = "Click here to see my LinkedIn profile.";
+}
